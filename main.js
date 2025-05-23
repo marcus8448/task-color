aec7e013c8f169e8b3568d66b3efca21b5bfec41
Marcus Low <mlow16@outlook.com>
2025-05-23 15:27:10 -0700

    feat: improve styling of colour picker input, preload default task colour on edit

--- main.js
+++ main.js
@@ -26,51 +26,42 @@ let editObserver = null;
 function onElementAdded(node) {
     if (node.nodeType === Node.ELEMENT_NODE) {
         if (node.getAttribute("aria-labelledby") === "tabTask") { // task creation dialog
-            // add colour selector to new task screen
-            let input = document.createElement("input");
-            input.type = "color";
-            input.oninput = event => {
-                pendingColour = event.target.value;
-                pendingCreation = true;
-                // update event colour in realtime
-                if (creatingEvent != null) {
-                    let target = getEventChip(creatingEvent);
-                    if (target) colourEventChip(pendingColour, target);
-                }
-            }
-            input.style.marginLeft = "4.25rem"
-            input.style.marginTop = "0.5rem"
-            node.appendChild(input)
-        } else if (node.hasAttribute("data-taskid")) { // select task view dialog
+            node.appendChild(createColourPickerRow())
+        } else if (node.hasAttribute("data-taskid")) { // select task view dialog (one click)
             if (node.parentElement != null
                 && node.parentElement.parentElement != null
                 && node.parentElement.parentElement.getAttribute("role") === "dialog") {
                 if (editObserver != null) {
-                    editObserver.nextDisconnect = true;
+                    editObserver.expire = true;
                     editObserver = null;
                 }
 
                 let taskId = node.getAttribute("data-taskid")
-                if (localStorage.getItem("task_color_" + taskId) != null) {
-                    // colour the dot in the task view dialog (one click)
-                    node.childNodes.item(1).firstChild.firstChild.firstChild.firstChild.style.backgroundColor = localStorage.getItem("task_color_" + taskId)
+                let chipNode = node.childNodes.item(1).firstChild.firstChild.firstChild.firstChild;
+                let baseColour;
+                if (hasTaskColour(taskId)) {
+                    // colour the dot in the task view dialog
+                    baseColour = getTaskColour(taskId)
+                    chipNode.style.backgroundColor = baseColour;
+                } else { // get default task colour
+                    baseColour = parseCssColour(chipNode.style.backgroundColor)
                 }
 
                 // create edit colour button
                 let input = document.createElement("input");
                 input.type = "color";
-                if (localStorage.getItem("task_color_" + taskId) != null) {
-                    input.value = localStorage.getItem("task_color_" + taskId);
+                input.value = baseColour;
+
+                let taskChip = getTaskChip(taskId);
+                if (taskChip != null && hasTaskColour(taskId)) {
                     // in week view, style is manually updated on click (with timed tasks) - counteract changes
-                    colourEventChip(input.value, getTaskChip(taskId))
+                    colourEventChip(taskChip, input.value)
 
                     let observer = new MutationObserver(_ => {
-                        colourEventChip(input.value, getTaskChip(taskId))
-                        if (observer.nextDisconnect) {
-                            observer.disconnect();
-                        }
+                        colourEventChip(taskChip, input.value)
+                        if (observer.expire) observer.disconnect();
                     });
-                    observer.observe(getTaskChip(taskId), {
+                    observer.observe(taskChip, {
                         attributes: true,
                         attributeFilter: ["style"],
                     });
@@ -78,8 +69,9 @@ function onElementAdded(node) {
                 }
 
                 input.oninput = event => {
-                    localStorage.setItem("task_color_" + taskId, event.target.value);
-                    colourEventChip(event.target.value, getTaskChip(taskId));
+                    setTaskColour(taskId, event.target.value);
+                    colourEventChip(taskChip, event.target.value);
+                    chipNode.style.backgroundColor = event.target.value;
                 }
                 input.style.marginLeft = "1rem"
                 input.style.marginRight = "0.25rem"
@@ -88,7 +80,7 @@ function onElementAdded(node) {
                 pendingCreation = false;
             }
         } else if (node.hasAttribute("data-eventid")) { // select event on calendar
-            // color task in on calendar
+            // colour task in on calendar
             tryColourEventButton(node, false)
         }
     }
@@ -111,8 +103,8 @@ function onElementRemoved(node) {
     node.childNodes.forEach(n => onElementAdded(n));
 }
 
-function colourEventChip(colour, target) {
-    if (colour != null) {
+function colourEventChip(target, colour) {
+    if (colour != null && target != null) {
         if (target.firstChild?.firstChild?.firstChild?.nodeName === "DIV") {
             target.firstChild.firstChild.firstChild.style.borderColor = colour;
         } else {
@@ -130,23 +122,23 @@ function colourEventChip(colour, target) {
     }
 }
 
-function tryColourEventButton(target, set) {
+function tryColourEventButton(target, saveColour) {
     let eventId = target.getAttribute("data-eventid");
     if (eventId.length === 52) { // events being created have a longer id
         creatingEvent = eventId
     } else if (eventId.startsWith("tasks_") // check if the event is a task
         && eventId !== "tasks_rollover_view" // check that the button is not a rollover group (past due date)
         && target.hasAttribute("data-eventchip")) {
-        eventId = eventId.substring("tasks_".length); // task id starts after tasks_ prefix
-        if (pendingCreation && pendingColour != null && set) {
-            localStorage.setItem("task_color_" + eventId, pendingColour);
+        let taskId = eventId.substring("tasks_".length); // task id starts after tasks_ prefix
+        if (pendingCreation && pendingColour != null && saveColour) {
+            setTaskColour(taskId, pendingColour);
             pendingCreation = false;
             pendingColour = null;
             creatingEvent = null;
         }
-        if (localStorage.getItem("task_color_" + eventId) != null) {
-            let colour = localStorage.getItem("task_color_" + eventId)
-            colourEventChip(colour, target);
+
+        if (hasTaskColour(taskId)) {
+            colourEventChip(target, getTaskColour(taskId));
         }
     }
 }
@@ -156,7 +148,71 @@ function getTaskChip(taskId) {
 }
 
 function getEventChip(eventId) {
-    return document.querySelector("[data-eventid=" + eventId + "]");
+    return document.querySelector("[data-eventchip][data-eventid=" + eventId + "]");
+}
+
+function createColourPickerRow() {
+    // <div style="display: flex; flex-direction:row; margin-top: 0.5rem; align-items: center;">
+    //     <div style="width: 1em; height: 1em; border-radius: 0.25em; background-color: green; margin-left: 1.75rem; margin-right: 1.6rem"></div>
+    //     <input type="color">
+    // </div>
+
+    let container = document.createElement("div");
+    container.style.display = "flex";
+    container.style.flexDirection = "row";
+    container.style.marginTop = "0.5rem";
+    container.style.alignItems = "center";
+
+    let paletteIcon = document.createElement("i");
+    paletteIcon.className = "google-material-icons";
+    paletteIcon.textContent = "palette";
+    paletteIcon.style.color = "var(--gm3-sys-color-on-surface-variant)";
+    paletteIcon.style.fontSize = "20px";
+    paletteIcon.style.pointerEvents = "none";
+    paletteIcon.style.userSelect = "none";
+    paletteIcon.style.marginLeft = "1.5rem";
+    paletteIcon.style.marginRight = "1.5rem";
+    container.appendChild(paletteIcon);
+
+    let picker = document.createElement("input");
+    picker.type = "color"
+    picker.value = undefined
+    picker.oninput = event => {
+        pendingColour = event.target.value;
+        pendingCreation = true;
+        // update event colour in realtime
+        if (creatingEvent != null) {
+            colourEventChip(getEventChip(creatingEvent), pendingColour);
+        }
+    }
+    container.appendChild(picker);
+
+    return container;
+}
+
+function getTaskColour(taskId) {
+    return localStorage.getItem("task_color_" + taskId);
+}
+
+function hasTaskColour(taskId) {
+    return getTaskColour(taskId) !== null;
+}
+
+function setTaskColour(taskId, colour) {
+    return localStorage.setItem("task_color_" + taskId, colour);
+}
+
+function parseCssColour(colourString) {
+    if (colourString.startsWith("rgb(")) {
+        let colour = 0;
+        colourString.substring(4, colourString.length-1).split(", ").forEach(c => {
+            colour = colour << 8;
+            colour |= Number.parseInt(c);
+        })
+        return "#" + colour.toString(16);
+    } else if (colourString.startsWith("#")) {
+        return colourString
+    }
 }
 
 const observer = new MutationObserver(mutations => {
@@ -168,6 +224,7 @@ const observer = new MutationObserver(mutations => {
             if (mutation.attributeName === "data-eventid") {
                 tryColourEventButton(mutation.target, true);
             } else if (mutation.attributeName === "data-start-date-key" || mutation.attributeName === "data-end-date-key") {
+                // view is changing - invalidate event creation
                 pendingCreation = false;
                 pendingColour = null;
                 creatingEvent = null;
